4580df605712482b7776203e69f5ed70d7f65f90
Bart Baas <info@baasgeo.com>
2015-08-19 09:52:01 +0200

    Cleanup

--- ol3-pinpoint.js
+++ ol3-pinpoint.js
@@ -2,8 +2,6 @@
  * OpenLayers 3 Layer Pinpoint Control.
  * @constructor
  * @extends {ol.control.Control}
- * @param {Object} opt_options Control options, extends olx.control.ControlOptions adding:
- *                              **`tipLabel`** `String` - the button tooltip.
  */
 ol.control.PinPoint = function(opt_options) {
 
@@ -15,11 +13,6 @@ ol.control.PinPoint = function(opt_options) {
     var element = document.createElement('div');
     element.className = 'ol-pinpoint ol-unselectable';
     element.appendChild(anchor);
-
-    //element.onclick = function(e) {
-    //    console.log('click pinpoint', e);
-    //    var map = this.getMap();
-    //};
 
     ol.control.Control.call(this, {
         element: element,
